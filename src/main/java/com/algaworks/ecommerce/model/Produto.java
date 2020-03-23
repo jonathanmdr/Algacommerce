3ffeea528890ccd03a107c1411251bf3183c455e
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-23 18:16:51 -0300

    Primeiro teste de JPA, buscando um produto

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -1,4 +1,27 @@
 package com.algaworks.ecommerce.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Produto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Integer id;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal preco;
+    
 }
