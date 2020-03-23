5f5d61cf076d4de068262e2fe313079890f35bff
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-23 20:20:33 -0300

    Testando a remoção de um obejto

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
-    public void inserirOPrimeiroObjeto() {
+    public void inserirProduto() {
         Produto produto = new Produto();
         produto.setId(2);
         produto.setNome("Câmera Canon");
@@ -25,6 +25,18 @@ public class OperacoesComTransacaoTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assert.assertNotNull(String.format("Produto de id: %d não foi inserido na base de dados", produtoVerificacao.getId()), produtoVerificacao);
+    }
+
+    @Test
+    public void removerProduto() {
+        Produto produto = entityManager.find(Produto.class, 3);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNull(String.format("Produto de id: %d não foi excluído", produto.getId()), produtoVerificacao);
     }
 
     /**
