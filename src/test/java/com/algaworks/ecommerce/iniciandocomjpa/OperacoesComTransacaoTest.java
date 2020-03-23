6bb153d2287127cd379583d62ec06b9baae6dddd
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-23 18:55:08 -0300

    Exemplificando uma transação

---
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -0,0 +1,25 @@
+package com.algaworks.ecommerce.iniciandocomjpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Test;
+
+public class OperacoesComTransacaoTest extends EntityManagerTest {
+
+    /**
+     * Método somente para exemplificar uma transação
+     */
+    @Test
+    public void abrirEFecharATransacao() {
+//        Produto produto = new Produto();
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.persist(produto);
+//        entityManager.merge(produto);
+//        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+    }
+
+}
