7eafc4e2988133eed7a1ff7208b51940ac26da1d
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-24 17:45:14 -0300

    Gerenciando transações

---
+++ src/test/java/com/algaworks/ecommerce/conhecendoentitymanager/GerenciamentoTransacoesTest.java
@@ -0,0 +1,32 @@
+package com.algaworks.ecommerce.conhecendoentitymanager;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.StatusPedido;
+import org.junit.Test;
+
+public class GerenciamentoTransacoesTest extends EntityManagerTest {
+
+    @Test(expected = Exception.class)
+    public void abrirFecharTransacao() {
+        try {
+            entityManager.getTransaction().begin();
+            metodoDeRegraDeNegocio();
+            entityManager.getTransaction().commit();
+        } catch(Exception ex) {
+            entityManager.getTransaction().rollback();
+            throw ex;
+        }
+    }
+
+    private void metodoDeRegraDeNegocio() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        pedido.setStatus(StatusPedido.PAGO);
+
+        if (pedido.getPagamentoCartao() == null) {
+            throw new RuntimeException("Pagamento o pedido ainda não foi efetuado");
+        }
+    }
+
+}
