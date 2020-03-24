ba6223d23df614a3954efd3ca99b91fcb3004747
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-24 14:30:38 -0300

    Entendendo o Auto relacionamento

--- src/main/java/com/algaworks/ecommerce/model/Categoria.java
+++ src/main/java/com/algaworks/ecommerce/model/Categoria.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -18,7 +19,11 @@ public class Categoria {
 
     private String nome;
 
-    @Column(name = "categoria_pai_id")
-    private Integer categoriaPaiId;
+    @ManyToOne
+    @JoinColumn(name = "categoria_pai_id")
+    private Categoria categoriaPai;
+
+    @OneToMany(mappedBy = "categoriaPai")
+    private List<Categoria> categorias;
 
 }
---
+++ src/test/java/com/algaworks/ecommerce/relacionamentos/AutoRelacionamentoTest.java
@@ -0,0 +1,36 @@
+package com.algaworks.ecommerce.relacionamentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AutoRelacionamentoTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamentoDeClienteComPedido() {
+        Categoria categoriaPai = new Categoria();
+        categoriaPai.setNome("Eletrônicos");
+
+        Categoria categoriaFilha = new Categoria();
+        categoriaFilha.setNome("Celulares");
+        categoriaFilha.setCategoriaPai(categoriaPai);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoriaPai);
+        entityManager.persist(categoriaFilha);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaFilhaVerificacao = entityManager.find(Categoria.class, categoriaFilha.getId());
+        Assert.assertNotNull(String.format("Categoria de id: %d não tem categoria pai vinculada", categoriaFilha.getId()), categoriaFilhaVerificacao);
+
+        Categoria categoriaPaiVerificacao = entityManager.find(Categoria.class, categoriaPai.getId());
+        Assert.assertFalse(String.format("Categoria de id: %d não tem categorias associadas", categoriaPai.getId()), categoriaPaiVerificacao.getCategorias().isEmpty());
+    }
+
+}
