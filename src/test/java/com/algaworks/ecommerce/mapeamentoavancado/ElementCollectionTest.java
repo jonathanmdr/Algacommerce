701a6567963a8b585357fc91783e6d72fa228c31
Jonathan Henrique <jonathan.mdr@hotmail.com>
2020-03-25 16:35:43 -0300

    Utilizando a anotação @ElementCollection com atributo do tipo Map

--- src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -2,11 +2,13 @@ package com.algaworks.ecommerce.mapeamentoavancado;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.model.Atributo;
+import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ElementCollectionTest extends EntityManagerTest {
 
@@ -34,6 +36,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assert.assertFalse("Lista de atributos está vazia", produtoVerificacao.getAtributos().isEmpty());
+    }
+
+    @Test
+    public void aplicarContatos() {
+        entityManager.getTransaction().begin();
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        cliente.setContatos(Collections.singletonMap("email", "fernando@email.com"));
+
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertFalse("Lista de contatos está vazia", clienteVerificacao.getContatos().isEmpty());
+        Assert.assertEquals("E-mail retornado não é igual ao esperado", "fernando@email.com", clienteVerificacao.getContatos().get("email"));
     }
 
 }
