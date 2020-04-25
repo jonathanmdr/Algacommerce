3d0842272c2ce819a0fd25df72d87266e4f9f227
Jonathan Henrique Medeiros <jonathan.medeiros@br.experian.com>
2020-04-25 18:24:15 -0300

    Customizando o DDL com a anotação @Column

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -14,10 +14,13 @@ import java.util.List;
        indexes = { @Index(name = "idx_nome", columnList = "nome") })
 public class Produto extends EntidadeBaseInteger {
 
+    @Column(length = 100, nullable = false)
     private String nome;
 
+    @Column(columnDefinition = "varchar(275) not null default 'descricao'")
     private String descricao;
 
+    @Column(precision = 10, scale = 2)
     private BigDecimal preco;
 
     @Lob
